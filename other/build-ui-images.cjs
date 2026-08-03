9d77dadf44c90b1acfe707ec280cf9d8226b5906
unknown <ugnius.valainis@gmail.com>
2026-08-03 11:25:52 +0300

    Serve responsive visual assets

--- other/build-ui-images.cjs
+++ other/build-ui-images.cjs
@@ -11,10 +11,27 @@ async function buildLosslessDisplayImage(source, output, width) {
     .toFile(path.join(imageRoot, output));
 }
 
-Promise.all([
-  buildLosslessDisplayImage("rockguy.png", "rockguy-loader.webp", 512),
-  buildLosslessDisplayImage("fih.png", "fih-ui.webp", 384)
-]).catch((error) => {
+const variants = [
+  ["rockguy.png", "rockguy-loader-128.webp", 128],
+  ["rockguy.png", "rockguy-loader-192.webp", 192],
+  ["rockguy.png", "rockguy-loader-320.webp", 320],
+  ["rockguy.png", "rockguy-loader.webp", 512],
+  ["fih.png", "fih-ui-128.webp", 128],
+  ["fih.png", "fih-ui-256.webp", 256],
+  ["fih.png", "fih-ui.webp", 384],
+  ["hero-pull-static.png", "hero-pull-static-256.webp", 256],
+  ["hero-pull-static.png", "hero-pull-static-320.webp", 320],
+  ["hero-pull-moving.png", "hero-pull-moving-128.webp", 128],
+  ["hero-pull-moving.png", "hero-pull-moving-160.webp", 160],
+  ["hero-figure-lower.png", "hero-figure-lower-384.webp", 384],
+  ["hero-figure-lower.png", "hero-figure-lower-512.webp", 512],
+  ["hero-figure-lower.png", "hero-figure-lower-768.webp", 768],
+  ["hero-figure-lower.png", "hero-figure-lower-896.webp", 896]
+];
+
+Promise.all(variants.map(([source, output, width]) => (
+  buildLosslessDisplayImage(source, output, width)
+))).catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
